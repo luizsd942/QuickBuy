bfb0418ff053b3806de94c1989659ed7ae83e14c
luizsd942 <luizsd942@gmail.com>
2019-09-21 22:19:06 -0300

    Criação de UsuarioServico para realizar chamadas ao WebApi no controller para Login.

--- QuickBuy.Web/ClientApp/src/app/cliente/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/cliente/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../modelo/usuario";
 import { Router, ActivatedRoute } from "@angular/router";
 import { state } from "@angular/animations";
+import { UsuarioServicos } from "../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "app-login",
@@ -14,7 +15,8 @@ export class LoginComponent implements OnInit {
   public usuario;  
   public urlRetorno: "";
 
-  constructor(private router: Router, private activedRoute: ActivatedRoute) {    
+  constructor(private router: Router, private activedRoute: ActivatedRoute,
+    private usuarioServico: UsuarioServicos) {    
   }
 
   ngOnInit(): void {
@@ -23,10 +25,20 @@ export class LoginComponent implements OnInit {
   }
 
   public login() {
-    if (this.usuario.email === 'luizsd942@gmail.com' && this.usuario.senha === 'abc123') {
-      sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate([this.urlRetorno]);
-    }
+    this.usuarioServico.verificarUsuario(this.usuario).
+      subscribe(
+        data => {
+
+        },
+        err => {
+
+        }
+      );
+
+    //if (this.usuario.email === 'luizsd942@gmail.com' && this.usuario.senha === 'abc123') {
+    //  sessionStorage.setItem("usuario-autenticado", "1");
+    //  this.router.navigate([this.urlRetorno]);
+    //}
 
   }
 
---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody] Usuario usuario )
+        {
+            try
+            {
+                if (usuario.Email == "luiz@teste.com" && usuario.Senha=="abc123")
+                    return Ok(usuario);
+                return BadRequest("Usuário ou senha inválido");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
